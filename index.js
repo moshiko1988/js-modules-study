15a93d84d5b081e0e1f579dd0b7ad385f16ec631
Saad Rhoulam <saad@rhoulam.com>
2015-08-04 14:41:06 -0400

    Create base inline module script.

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// we want to do some work in this function
+// then return a module from it
+function f() {
+	// declare some variables in here that will be hidden from outside
+	var a = 0;
+
+	// `g` will have access to variables in this scope, such as `a`
+	function g() {
+		return a++;
+	}
+
+	// return the function so we can access it from outside
+	return g;
+}
+
+// we want to store our generated module in `module`
+var module = f(); // `module` now contains `g` from inside `f`!
+
+// from here, use the module you've created
+for(var index=0; index < 10; index++) {
+	var messageToBeLogged = module();
+	console.log(messageToBeLogged);
+}
