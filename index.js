fd0c7dc259078dfc8494f88afc4f0aa9d9d898d1
Saad Rhoulam <saad@rhoulam.com>
2015-08-04 15:21:12 -0400

    Add files.

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// this is how we import modules
+// for modules registered with npm, we merely give a name
+// for our own files, we give a relative path
+var ourModule = require('./module');
+
+// your module is available here as `ourModule`
+// use it!
+
---
+++ module.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// code to your heart's content
+// your goal is to have something at the end to show for it
+// rough goal: an object with a property containing a function
+// once you've got it, assign it to `module.exports` at the
+	// bottom of the file
+
+module.exports = null;
+
